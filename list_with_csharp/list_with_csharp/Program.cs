fe9c16e8cd83d3f2d24c8e67682e4cb91b1249c7
Lucas Machado <lukaasmachado143@gmail.com>
2022-08-09 21:14:48 -0300

    Iniciando estudos de listas em C#

---
+++ list_with_csharp/list_with_csharp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace list_with_csharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
