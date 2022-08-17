6c11484c5beea621f2abe7922ff5c44a8f9fbb56
Lucas Machado <lukaasmachado143@gmail.com>
2022-08-16 22:02:42 -0300

    Finalizando estudos sobre listas em C#

--- list_with_csharp/list_with_csharp/Program.cs
+++ list_with_csharp/list_with_csharp/Program.cs
@@ -11,6 +11,48 @@ namespace list_with_csharp
         static void Main(string[] args)
         {
             List<string> clients = new List<string>();
+            List<int> ages = new List<int>();
+
+            //for (int i = 0; i < 3; i++)
+            //{
+            //    Console.WriteLine($"Informe o nome da posição {i}:");
+            //    clients.Add(Console.ReadLine());
+            //}
+
+            //foreach(string client in clients)
+            //{
+            //    Console.WriteLine(client);
+            //}
+
+            //clients.RemoveAt(2);
+            //clients.RemoveAll(client => client == "Lucas");
+            //Console.WriteLine("*********************************************************");
+
+            //foreach (string client in clients)
+            //{
+            //    Console.WriteLine(client);
+            //}
+            //Console.ReadLine();
+            Console.WriteLine("*********************************************************");
+            
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("Informe um valor:");
+                ages.Add(int.Parse(Console.ReadLine()));
+            }
+            foreach(int age in ages)
+            {
+                Console.WriteLine(age);
+            }
+            Console.ReadLine();
+
+            //int search = ages.FindAll(age => age > 2);
+            //Console.WriteLine(search);
+
+            List<int> filteredAges = ages.FindAll(age => age > 2);
+            Console.WriteLine(filteredAges);
+            Console.ReadLine();
+
 
         }
     }
